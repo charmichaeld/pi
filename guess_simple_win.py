567b9e3e9fc33e5a681366cc18f9af936b0f73c7
charmichaeld <cmdickens@gmail.com>
2017-06-15 22:14:44 +0000

    add win

---
+++ guess_simple_win.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import random
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+####################################################
+# for buzzer
+buzz_pin = (11)
+GPIO.setup(buzz_pin,GPIO.OUT)   # Set buzz_pin's mode as output
+Buzz = GPIO.PWM(buzz_pin,440)
+
+# for led
+led_pin = (37)
+GPIO.setup(led_pin, GPIO.OUT)   # Set led_pin's mode as output
+#
+####################################################
+
+def win():
+    Buzz.start(50)
+    Buzz.ChangeFrequency(500)
+    time.sleep(0.2)
+    Buzz.stop()
+    Buzz.start(50)
+    Buzz.ChangeFrequency(600)
+    time.sleep(0.2)
+    Buzz.stop()
+
+n = random.randint(1,10)
+
+guess = int(raw_input("Enter an number from 1 to 10:"))
+while n != "guess":
+    print
+    if guess < n:
+        print "Guess is too low"
+        Buzz.start(50)
+        Buzz.ChangeFrequency(100)
+        time.sleep(1)
+        Buzz.stop()
+        guess = int(raw_input("Enter an number from 1 to 10:"))
+    elif guess > n:
+        print "Guess is too high"
+        Buzz.start(50)
+        Buzz.ChangeFrequency(440)
+        time.sleep(1)
+        Buzz.stop()
+        guess = int(raw_input("Enter an number from 1 to 10:"))
+    else:
+        print "You guessed it!"
+        GPIO.output(led_pin, True)
+        for i in range(1, 10):
+            win()
+        GPIO.output(led_pin, False)
+        break
+    print
+
+GPIO.cleanup()
